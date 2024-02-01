1c1fa78c13328e9365ba1183a8d739f7d2a2f9b4
NG WEI HEN <rolandng0001@gmail.com>
2024-02-01 21:51:46 +0800

    Update for early code preparation

---
+++ Controller Layer/User/Forget Password Process.php
@@ -0,0 +1,35 @@
+<!--================================================================
+    USER FORGET PASSWORD PROCESS: Handles user forget password
+==================================================================->
+<?php 
+
+    /**
+     * import database connection file 
+     */
+
+    // Connection 1: Using MySQLi Object Oriented 
+    include ('../../Database Layer/db_connection_MySQLi ObjectOriented.php');
+    // Connection 2: Using MySQLi Procedural
+    include ('../../Database Layer/db_connection_MySQLi Procedural.php');
+    // Connection 3: Using MySQLi PDO
+    include ('../../Database Layer/db_connection_MySQLi PDO.php');
+
+    //-------------------------------------------------------------------
+
+    /**
+     * Method 1: using MySQLi Object Oriented
+     */
+
+    
+    /**
+     * Method 2: using MySQLi Procedural
+     */
+
+
+    /**
+     * Method 3: using MySQLi PDO
+     */
+
+    
+
+?>
