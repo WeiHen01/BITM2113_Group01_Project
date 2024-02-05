550f6260c3a737a62b8a08235044a7298a8352d5
NG WEI HEN <rolandng0001@gmail.com>
2024-02-05 19:45:57 +0800

    Update for early code preparation

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 
 ?>
 
-<!--========================================================================
-    index.php: Default page of the website once accessing through browser,
-               it is also the landing page of the website
-=========================================================================-->
+<!--=================================================================================
+    index.php: Default / Initial page of the website once accessing through browser,
+               it is also the landing page of the website.
+==================================================================================-->
 
 
 <!------------------------------
     FRONT-END OF THE WEBPAGE
 ------------------------------->
 
-<!--Telling browser using latest version of HTML - HTML5 -->
+<!--Telling browser using latest version of HTML documents - HTML5 -->
 <!DOCTYPE html>
 
     <!-- Import CSS References Stylesheets using URL Link-->
@@ -20,7 +20,14 @@
 
 
     <!-- Embedded CSS style -->
+    <style>
 
+    </style>
+
+    <!-- Import Javascript / JQuery References -->
+    <script>
+
+    </script>
 
 
     <!-- FavIcon on the browser tab-->
@@ -37,8 +44,15 @@
 
     <!-- Body of the webpage -->
     <body>
+        <!-- Sidebar / Vertical Drawer -->
+
 
         <!-- Horizontal Navigation bar -->
+
+
+        <!-- Content -->
+        <h1></h1>
+        <p></p>
 
     </body>
 
@@ -56,3 +70,8 @@
 <script>
 
 </script>
+
+<!-- Import Javascript / JQuery References -->
+
+
+<!-- Import CSS References Stylesheets using URL Link-->
