a18dac8aaf8bf34bfbf3f303ea924e8f2b3b3738
NG WEI HEN <rolandng0001@gmail.com>
2024-02-06 09:56:29 +0800

    Database Early Preparation

--- Database Layer/db_connection_MYSQLi Procedural.php
+++ Database Layer/db_connection_MYSQLi Procedural.php
@@ -6,8 +6,19 @@
     Connection Type 2: MySQLi Procedural
 =========================================================================-->
 
-<?php 
+<?php
 
-
-
-?>
+    $servername = "localhost";
+    $username = "username";
+    $password = "";
+    
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password);
+    
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    
+    echo "Connected successfully"; 
+?> 
--- Database Layer/db_connection_MySQLi ObjectOriented.php
+++ Database Layer/db_connection_MySQLi ObjectOriented.php
@@ -6,8 +6,19 @@
     Connection Type 1: MySQLi Object Oriented - Using preparedStatements
 =========================================================================-->
 
-<?php 
+<?php
+    
+    $servername = "localhost";
+    $username = "username";
+    $password = "";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+    
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
 
-
-
-?>
+    echo "Connected successfully"; 
+?> 
