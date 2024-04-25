317c879f57f71d73079211f6f5eb4e4b511384ee
NG WEI HEN <rolandng0001@gmail.com>
2024-04-25 15:06:10 +0800

    Update

--- Login.php
+++ Login.php
@@ -1,11 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>BITM2113 | Login</title>
+
+        <link href='https://fonts.googleapis.com/css?family=Epilogue:ExtraBold' rel='stylesheet'>
+        <link href='https://fonts.googleapis.com/css?family=Epilogue' rel='stylesheet'>
+
+        <!-- FavIcon on the browser tab-->
+        <link rel="icon" type="image/x-icon" href="./Assets/Image/logo.png">
+
+        <!-- FONT AWESOME ICON -->
+        <script src="https://kit.fontawesome.com/74a2be9f6d.js" crossorigin="anonymous"></script>
+    </head>
+    <style>
+        body{
+            background-image: url("./Assets/Image/water_bg_02.jpg");
+            background-size: 100vw;
+            max-height: 100vh;
+            overflow: hidden;
+            background-repeat: none;
+            font-family: 'Epilogue';
+            height: 100vh;
+        }
+        
+        /* Image 32 */
+        /* Container 21 */
+        .container {
+            margin: 2%;
+            padding: 2%;
+            width: 92%;
+            height: 84vh; /* Set the height of the container */
+            background: rgba(255, 255, 255, 0.5); /* white */
+            border-radius: 6px; /* border-m */
+            box-shadow: 0px 0px 1px #171a1f, 0px 0px 2px #171a1f; /* shadow-xs */
+        }
+
+        p { 
+             
+            width: 35vw; 
+            font-family: Epilogue; /* Heading */
+            color: #171A1FFF; /* neutral-900 */
+        }
+
+        .right-container{
+            display: flex;
+        }
+
+
+        
+
+    </style>
+    <body>
+        <div class="container">
+            
+            <p style="font-weight: 700; font-size: 48px; ">Welcome back! 👋</p>
+
+            <div class="right-container">
+
+            </div>
+
+        </div>
+
+        
+    </body>
 </html>
