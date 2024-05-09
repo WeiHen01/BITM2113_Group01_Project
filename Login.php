00a1cc505a4bff189fbfc7c82a99678fff08b087
NG WEI HEN <rolandng0001@gmail.com>
2024-05-09 13:25:00 +0800

    Updates

--- Login.php
+++ Login.php
@@ -38,31 +38,50 @@
             border-radius: 4px; /* border-m */
             box-shadow: 0px 0px 1px #171a1f, 0px 0px 2px #171a1f; /* shadow-xs */
         }
-        
 
-        /* Textbox 7 */
         .textbox {
-            position: absolute; 
-            left: 1%; 
-            display: flex;
-            flex-direction: column;
+            position: relative;
+            margin-bottom: 10px; /* Adjust as needed */
         }
 
+       
         .textbox input {
-            width: 35vw; 
-            height: 7vh;
-            padding-left: 44px; 
-            padding-right: 16px; 
-            font-family: Inter; 
-            font-size: 16px; 
-            line-height: 26px; 
+            font-family: 'Epilogue';
+            width: 41vw; 
+            height: 46px;
+            font-size: 14px; 
+            line-height: 22px; 
             font-weight: 400; 
             background: #F3F4F6FF; /* neutral-200 */
             border-radius: 8px; /* border-xl */
             border-width: 0px; 
             outline: none; 
+            padding-left: 35px; /* Adjust the padding to make space for the icon */
         }
+        /* hover */
+        .textbox input:hover {
+            font-family: 'Epilogue';
+            color: #000000; /* neutral-400 */
+            background: #F3F4F6FF; /* neutral-200 */
+        }
+        /* focused */
+        .textbox input:focused {
+            font-family: 'Epilogue';
+            color: #000000; /* neutral-400 */
+            background: #FFFFFFFF; /* white */
+        }
+        /* disabled */
+        .textbox input:disabled {
+            font-family: 'Epilogue';
+            color: #000000; /* neutral-400 */
+            background: #F3F4F6FF; /* neutral-200 */
+        }
+        .textbox :disabled + .icon, .textbox :disabled + .icon + .icon {
 
+            font-family: 'Epilogue';
+            fill: #000000; /* neutral-400 */
+        }
+        
         
 
     </style>
@@ -71,16 +90,19 @@
             <h1 style="font-family: Epilogue; font-size: 50px;">Welcome back 👋</h1>
             <h3 style="font-family: Epilogue; font-size: 24px; font-weight: 400">Log in your account</h3>
             <form>
+                <b>Your email</b>
                 <div class = "textbox">
-                    <b>Your email</b>
+                    <i style = "position: absolute; left: 5px; top: 13px" class="fas fa-envelope" id="icons-1"></i> <!-- Icon for email -->
                     <input type = "text">
                 </div>
 
-                <div style = "height: 15%"></div> 
+                <div style = "height: 2vh"></div>
 
+                <b>Password</b>
                 <div class = "textbox">
-                    <b>Password</b>
-                    <input type = "text">
+                    <i style = "position: absolute; left: 5px; top: 13px" class="fas fa-lock" id="icons-2"></i> <!-- Icon for password -->
+                    <input type = "text" name = "Password" id="passwordInput" placeholder = "Set Your Password" />
+                    <i style = "position: absolute; right: 25px; top: 13px" class="fas fa-eye" id="togglePassword"></i> <!-- Toggle button for password -->
                 </div>
 
                 
