0aa1759fd26817e94aee1b4a7ade48245597ff67
yanaazani <shaufyy48@gmail.com>
2024-05-24 18:17:25 +0800

    shao

--- View Layer/Organization/Org Home.php
+++ View Layer/Organization/Org Home.php
@@ -142,7 +142,6 @@
             box-shadow: 0px 17px 35px #171a1f, 0px 0px 2px #171a1f; /* shadow-xl */
             z-index: 1000;
         }
-
         /* Group 26 inside Popup container */
         .popup-group {
             position: absolute; 
@@ -179,7 +178,34 @@
             border-radius: 0px; 
             z-index: 999; /* Ensure it's above other elements */
         }
+        /* Popup container for adding a new event */
+        #newEventPopupContainer {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 100%; 
+            height: 100%;
+            background: #DEE1E6FF; /* neutral-300 */
+            border-radius: 15px; /* border-xl */
+            box-shadow: 0px 17px 35px #171a1f, 0px 0px 2px #171a1f; /* shadow-xl */
+            z-index: 1001; /* Ensure it's above other elements */
+            padding: 20px;
+            display: none; /* Hidden by default */
+        }
 
+        /* Overlay for new event popup container */
+        #overlayNewEventPopup {
+            position: fixed;
+            top: 0px;
+            left: 0px;
+            width: 100%;
+            height: 100%;
+            background: #171A1F66; /* neutral-900 */
+            border-radius: 0px;
+            z-index: 1000; /* Ensure it's above other elements */
+            display: none; /* Hidden by default */
+        }
 
     </style>
 
@@ -254,63 +280,119 @@
             </div>
         </div>
 
-       
+        <!-- Overlay 1-->
+        <div class="overlay" id="overlay" style="display:none;" onclick="hidePopup()"></div>
+            <!-- Popup container -->
+            <div class="popup-container" id="popupContainer">
+                <!-- Row 1 -->
+                <div class="rectangle" style="left: 5%" >
+                    <div class="sub-image">
+                            <!-- Image content -->
+                            <img src="../../Assets/Image/Org Water Droplets.png" alt="Image">
+                        </div>
+                    <div class="popup-group" style="top: 65%; left: 35%">
+                        <div class="sub-text" style="padding-top: 10px;">Have new event to launch?</div>
+                        <button class="popup-button" onclick="showAddNewEvent()">Add New Event</button>
+                    </div>
+                </div>
+                <!-- Row 2 -->
+                <div class="rectangle" style="left: 35%" >
+                    <div class="sub-image">
+                            <!-- Image content -->
+                            <img src="../../Assets/Image/Org Elemental.png" alt="Image">
+                        </div>
+                    <div class="popup-group" style="top: 65%; left: 35%">
+                        <div class="sub-text" style="padding-top: 10px;">Update and edit your event here!</div>
+                        <button class="popup-button">Edit Event</button>
+                    </div>
+                </div>
+                <!-- Row 3 -->
+                <div class="rectangle"  style="left: 65%">
+                    <div class="sub-image">
+                            <!-- Image content -->
+                            <img src="../../Assets/Image/Org View.png" alt="Image">
+                        </div>
+                    <div class="popup-group" style="top: 65%; left: 35%">
+                        <div class="sub-text" style="padding-top: 10px;">View all your event here!</div>
+                        <button class="popup-button">Event Calender</button>
+                    </div>
+        </div>
 
-        
+         <!-- Popup container for adding a new event -->
+        <div class="popup-container" id="newEventPopupContainer" style="display: flex; padding: 20px;
+            background: white; border-radius: 8px;">
+        <div style="background-image: url('../../Assets/Image/Org New Event.png'); 
+            padding: 20px; border-radius: 8px; width: 30%; background-size: cover; background-position: center;">
+            <h2 style="writing-mode: vertical-rl; text-align: center; 
+            font-size: 80px;  padding: 50px; transform: rotate(180deg); margin: 0;">New Event</h2>
+        </div>
 
-        <!-- Overlay -->
-        <div class="overlay" id="overlay" style="display:none" onclick="hidePopup()"></div>
-        <!-- Popup container -->
-        <div class="popup-container" id="popupContainer">
-            <!-- Row 1 -->
-            <div class="rectangle" style="left: 5%" >
-                <div class="sub-image">
-                        <!-- Image content -->
-                        <img src="../../Assets/Image/Org Water Droplets.png" alt="Image">
+            <div style="flex-grow: 1; padding: 20px;">
+                <!-- Event Title -->
+                <h3>Please enter details for new event:</h3>
+                <!-- Event Name -->
+                <input type="text" id="newEventName" name="newEventName" placeholder="Event Name" style="width: 100%; margin-bottom: 10px; padding: 10px; font-family: Inter; font-size: 16px; line-height: 24px; border: 1px solid #9095A0; border-radius: 4px;">
+
+                <!-- Event Description -->
+                <textarea id="newEventDescription" name="newEventDescription" placeholder="Description" rows="4" style="width: 100%; margin-bottom: 10px; padding: 10px; font-family: Inter; font-size: 16px; line-height: 24px; border: 1px solid #9095A0; border-radius: 4px;"></textarea>
+
+                <!-- Event Location -->
+                <select id="newEventLocation" name="newEventLocation" style="width: 100%; margin-bottom: 10px; padding: 10px; font-family: Inter; font-size: 16px; line-height: 24px; border: 1px solid #9095A0; border-radius: 4px;">
+                    <option value="" disabled selected>Location</option>
+                    <!-- Add your locations here -->
+                </select>
+
+                <!-- Event Category -->
+                <input type="text" id="newEventCategory" name="newEventCategory" placeholder="Category" style="width: 100%; margin-bottom: 10px; padding: 10px; font-family: Inter; font-size: 16px; line-height: 24px; border: 1px solid #9095A0; border-radius: 4px;">
+
+                <!-- Participation Quota -->
+                <div style="margin-bottom: 10px;">
+                    <label style="display: block; font-family: Inter; font-size: 16px; margin-bottom: 5px;">Participation Quota</label>
+                    <div>
+                        <input type="radio" id="quota1" name="participationQuota" value="<50">
+                        <label for="quota1" style="margin-right: 10px;">Participants < 50</label>
+                        <input type="radio" id="quota2" name="participationQuota" value="50-100">
+                        <label for="quota2" style="margin-right: 10px;">50 < Participants < 100</label>
+                        <input type="radio" id="quota3" name="participationQuota" value="100-250">
+                        <label for="quota3" style="margin-right: 10px;">100 < Participants < 250</label>
+                        <input type="radio" id="quota4" name="participationQuota" value="others">
+                        <label for="quota4">Others</label>
                     </div>
-                <div class="popup-group" style="top: 65%; left: 35%">
-                    <div class="sub-text" style="padding-top: 10px;">Have new event to launch?</div>
-                    <button class="popup-button">Add New Event</button>
+                    <textarea id="otherQuota" name="otherQuota" placeholder="Others" rows="1" style="width: 100%; margin-top: 10px; padding: 10px; font-family: Inter; font-size: 16px; line-height: 24px; border: 1px solid #9095A0; border-radius: 4px;"></textarea>
                 </div>
-            </div>
-            <!-- Row 2 -->
-            <div class="rectangle" style="left: 35%" >
-                <div class="sub-image">
-                        <!-- Image content -->
-                        <img src="../../Assets/Image/Org Elemental.png" alt="Image">
+
+                <!-- Button to submit event --> 
+                <button class="popup-button" onclick="submitNewEvent()" style="width: 100%; padding: 10px; background-color: #2979FF; color: white; font-family: Inter; font-size: 16px; line-height: 24px; border: none; border-radius: 4px;">Add New Event</button>
                     </div>
-                <div class="popup-group" style="top: 65%; left: 35%">
-                    <div class="sub-text" style="padding-top: 10px;">Update and edit your event here!</div>
-                    <button class="popup-button">Edit Event</button>
-                </div>
-            </div>
-            <!-- Row 3 -->
-            <div class="rectangle"  style="left: 65%">
-                <div class="sub-image">
-                        <!-- Image content -->
-                        <img src="../../Assets/Image/Org View.png" alt="Image">
+                    <div style="padding: 20px; border-radius: 8px; width: 40%;">
+                        <div style="background-image: url('../../Assets/Image/Org2.png'); padding: 80px; border-radius: 8px; position: relative;">
+                            <div style="position: absolute; top: 7%; right: 40%; background: #987070 ;color: white; border-radius: 50%; padding: 5px 10px;">?</div>
+                            <p style="font-family: Inter; font-size: 16px; line-height: 24px; text-align: center; color: #F1EEDC;">"The environment is where we all meet; where we all have a mutual interest; it is the one thing all of us share."</p>
+                        </div>
                     </div>
-                <div class="popup-group" style="top: 65%; left: 35%">
-                    <div class="sub-text" style="padding-top: 10px;">View all your event here!</div>
-                    <button class="popup-button">Event Calender</button>
                 </div>
-            </div>
+
+        <!-- Overlay for new event popup container -->
+        <div class="overlay" id="overlayNewEventPopup" style="display: none;" onclick="hidePopup()"></div>
+
+
     </body>
         <script>
-            // Function to show the popup container and overlay
             function showPopup() {
-                // Display the popup container
                 document.getElementById("popupContainer").style.display = "block";
-                // Display the overlay
                 document.getElementById("overlay").style.display = "block";
             }
-            // Function to hide the popup container and overlay
             function hidePopup() {
-                // Hide the popup container
                 document.getElementById("popupContainer").style.display = "none";
-                // Hide the overlay
                 document.getElementById("overlay").style.display = "none";
             }
+        </script>
+        <script>
+            function showAddNewEvent() {
+                document.getElementById("newEventPopupContainer").style.display = "block";
+                document.getElementById("overlayPopup").style.display = "block";
+            }
+
         </script>
         <script src="../General Components & Widget/User/User Component Script.js"></script>
 
