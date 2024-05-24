94df15d2044e342032643d4e3367505d3d809663
yanaazani <shaufyy48@gmail.com>
2024-05-24 12:28:03 +0800

    shao

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <!DOCTYPE html>
 
     <!-- External CSS style -->
-    <link href='https://fonts.googleapis.com/css?family=Epilogue:ExtraBold' rel='stylesheet'>
+    <link href="./output.css" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Epilogue' rel='stylesheet'>
 
     <!-- Import Javascript / JQuery References -->
@@ -40,19 +40,14 @@
             html{
                 font-family: 'Epilogue';
             }
-            body{
-                font-family: 'Epilogue';
-            }
              /* Button 1 */
             .button-1 {
-                font-family: 'Epilogue';
                 position: absolute; 
-                width: 87px; 
-                height: 40px; 
                 padding: 0 20px; 
                 display: flex; 
                 align-items: center; 
                 justify-content: center; 
+                font-family: Inter; 
                 font-size: 15px; 
                 line-height: 28px; 
                 font-weight: 400; 
@@ -66,16 +61,14 @@
             }
             /* Button 2 */
             .button-2 {
-                font-family: 'Epilogue';
                 position: absolute; 
-                top: 19px; 
-                left: 1269px; 
                 width: 148px; 
                 height: 40px; 
                 padding: 0 20px; 
                 display: flex; 
                 align-items: center; 
                 justify-content: center; 
+                font-family: Inter; 
                 font-size: 15px; 
                 line-height: 28px; 
                 font-weight: 400; 
@@ -84,100 +77,142 @@
                 opacity: 1; 
                 border: none; 
                 border-radius: 8px; /* border-xl */
-                border-color: #00BDD6FF; /* primary-500 */
             }
-
-            .header-menu {
-                font-family: 'Epilogue';
+            .button {
                 position: absolute; 
-                top: 3%; 
-                left: 50%; 
-                transform: translateX(-50%);
-                width: 25%; 
-                height: 6%; 
-                display: flex; 
-                align-items: center; 
-                font-family: Inter; /* Body */
-                font-size: 1.1%; 
-                line-height: 1.8%; 
-                font-weight: 400; 
-                opacity: 1; 
-            }
-            .header-menu .header-menu-item {
-                font-family: 'Epilogue';
-                padding: 2% 5%; 
+                width: 175px; 
+                height: 50px; 
+                padding: 0 20px; 
                 display: flex; 
                 align-items: center; 
                 justify-content: center; 
-                color: #565E6CFF; /* neutral-600 */
-                background: transparent; 
+                font-family: Inter; 
+                font-size: 18px; 
+                line-height: 28px; 
+                font-weight: 400; 
+                color: #171A1FFF; /* neutral-900 */
+                background: #FFFFFFFF; /* white */
+                opacity: 1; 
+                border: none; 
                 border-radius: 4px; /* border-m */
-                cursor: pointer; 
-                white-space: nowrap; 
+                gap: 8px; 
+            }
+            .header-menu {
+            position: absolute; 
+            top: 3%; 
+            left: 50%; 
+            transform: translateX(-50%);
+            width: 25%; 
+            height: 6%; 
+            display: flex; 
+            align-items: center; 
+            font-family: Inter; /* Body */
+            font-size: 1.1%; 
+            line-height: 1.8%; 
+            font-weight: 400; 
+            opacity: 1; 
+            }
+            .header-menu .header-menu-item {
+            padding: 2% 5%; 
+            display: flex; 
+            align-items: center; 
+            justify-content: center; 
+            color: #565E6CFF; /* neutral-600 */
+            background: transparent; 
+            border-radius: 4px; /* border-m */
+            cursor: pointer; 
+            white-space: nowrap; 
             }
             .header-menu .header-menu-item.selected {
-                position: relative; 
-                font-weight: 700; 
-                color: #00BDD6FF; /* primary-500 */
-                background: transparent; 
-            }
-
-            .header-menu .header-menu-item:nth-child(1):hover {
-                color: #00BDD6FF; /* primary-500 */
-                background: transparent; 
-            }
-            .header-menu .header-menu-item:nth-child(2):hover {
-                color: #00BDD6FF; /* primary-500 */
-                background: transparent; 
-            }
-            .header-menu .header-menu-item:nth-child(3):hover {
-                color: #00BDD6FF; /* primary-500 */
-                background: transparent; 
-            }
-            .header-menu .header-menu-item:nth-child(4):hover {
-                color: #00BDD6FF; /* primary-500 */
-                background: transparent; 
+            position: relative; 
+            font-weight: 700; 
+            color: #00BDD6FF; /* primary-500 */
+            background: transparent; 
             }
             .header-menu .header-menu-item.selected:hover:active {
-                color: #00BDD6FF; /* primary-500 */
-                background: transparent; 
+            color: #00BDD6FF; /* primary-500 */
+            background: transparent; 
             }
             .header-menu .header-menu-item:nth-child(1) {
-            
+            font-family: Inter; 
             font-size: 15px; 
             line-height: 1.8%; 
             font-weight: 400; 
             color: #565E6CFF; /* neutral-600 */
             }
             .header-menu .header-menu-item:nth-child(2) {
-            
+            font-family: Inter; 
             font-size: 15px; 
             line-height: 1.8%; 
             font-weight: 400; 
             color: #565E6CFF; /* neutral-600 */
             }
             .header-menu .header-menu-item:nth-child(3) {
-                
-                font-size: 15px; 
-                line-height: 1.8%; 
-                font-weight: 400; 
-                color: #565E6CFF; /* neutral-600 */
+            font-family: Inter; 
+            font-size: 15px; 
+            line-height: 1.8%; 
+            font-weight: 400; 
+            color: #565E6CFF; /* neutral-600 */
             }
             .header-menu .header-menu-item:nth-child(4) {
-            
-                font-size: 15px; 
-                line-height: 1.8%; 
-                font-weight: 400; 
-                color: #565E6CFF; /* neutral-600 */
+            font-family: Inter; 
+            font-size: 15px; 
+            line-height: 1.8%; 
+            font-weight: 400; 
+            color: #565E6CFF; /* neutral-600 */
             }
 
+             /* Heading Title */
             .text {
                 font-family: Epilogue; /* Heading */
                 line-height: 78px; 
                 font-weight: 700; 
                 color: #171A1FFF; /* neutral-900 */
-                text-align: center;
             }
+            /* Body Title */
+            .text-body {
+                font-family: Inter; /* Body */
+                line-height: 30px; 
+                color: #9095A0FF; /* neutral-500 */
+            }
+            /* Normal Text */
+            .text-normal {
+                position: absolute; 
+                top: 212px; 
+                left: 290px; 
+                width: 880px; 
+                font-family: Inter; /* Body */
+                font-size: 32px; 
+                line-height: 30px; 
+                font-weight: 400; 
+                color: #171A1FFF; /* neutral-900 */
+            }
+
+            .image-container {
+                display: flex;
+                justify-content: center; 
+                align-items: center; 
+                gap: 7%; 
+                height: 20vh; 
+            }
+            .image-container img {
+                width: 10%;
+                max-width: 15%; 
+                height: auto;
+                border-radius: 30px; 
+            }
+
+            /* Container  */
+            .container {
+                align-items: center;
+                left: 1px; 
+                width: 1440px; 
+                height: 50px; 
+                background: #FFFFFFFF; /* white */
+                border-radius: 0px; 
+            }
+
+
         </style>
 
     <!-- Head of the webpage -->
@@ -201,17 +236,13 @@
         
     </head>
 
-
     <!-- Body of the webpage -->
     <body>
-            
-        <!-- Sidebar / Vertical Drawer -->
-
 
          <!--=============
                Header
            ==============-->
-           <div id="logo" style="display: flex; align-items: center; cursor: pointer; justify-content: left;">
+        <div id="logo" style="display: flex; align-items: center; cursor: pointer; justify-content: left;">
             <img src="Assets/Image/H20 Harmony Logo.png" alt="Logo" width="50">
             <p style="font-size: 15px;"><b>H2O Harmony</b></p>
         </div>
@@ -223,16 +254,96 @@
             <div class="header-menu-item">Learn More</div>
         </div>
 
-        <button class="button-1" onclick="window.location.href='./Login.php'", style=" top: 19px; left: 80%; margin-right: 1%" onmouseover="this.style.backgroundColor='#00BDD6FF'; this.style.color = '#ffffff'; this.style.cursor = 'pointer'" onmouseout="this.style.backgroundColor='#ffffff'; this.style.color='#00BDD6FF'">Login</button>
-        
-        <button class="button-2" onclick="window.location.href='./Register.php'", style="top: 19px; left: 86%; margin-left: 1%;" onmouseover="this.style.backgroundColor='transparent'; this.style.color = '#00BDD6FF'; this.style.cursor = 'pointer'" onmouseout="this.style.backgroundColor='#00BDD6FF'; this.style.color='#ffffff';">Get Started!</button>
+        <button class="button-1" onclick="window.location.href='./Login.php'", style=" top: 5%; left: 83%; width: 87px; height: 40px; ">Login</button>
+        <button class="button-2" onclick="window.location.href='./Register.php'", style="top: 5%; left: 89%; ">Get Started!</button>
 
         <!--=============
                Title
            ==============-->
-           <div class="text" style=" top: 60px; left: 118px; font-size: 56px; ">Pure Waters, Brighter Futures</div>
-           <div class="text" style=" top: 110px; left: 118px; font-size: 56px; ">Partner with Us for Clean Water Solutions!</div>
-    </body>
+           <div class="text" style=" top: 100px; left: 118px; font-size: 56px; text-align: center;">Pure Waters, Brighter Futures</div>
+           <div class="text" style=" top: 140px; left: 118px; font-size: 56px; text-align: center; ">Partner with Us for Clean Water Solutions!</div>
+           <div class="text-body" style=" top: 200px; left: 200px; font-size: 20px; font-weight: 400; text-align: center;">Empowering Communities through Innovative 
+           and Sustainable Water Solutions, <br>
+           Ensuring Access to Clean Water for a 
+           Healthier and Brighter Future for All</div>
+
+        <!--=====================
+            Upper-Body Content
+        =========================-->
+            <!-- Image content -->
+            <div class="image-container" style="display: flex; justify-content: center; align-items: center; height: 80vh;">
+                <img src="Assets/Image/ocean.png" style="width: 100%; max-width: 1308px; height: auto; border-radius: 30px;" alt="Event Image">
+            </div>
+
+            <div class="text-body" style=" top: 200 px; left: 200px; font-size: 30px; font-weight: 600; text-align: center;">Trusted by</div>
+
+            <div class="image-container">
+                <img src="Assets/Image/water org.png" alt="Water Org Image">
+                <img src="Assets/Image/uniceff.png" alt="Unicef Image">
+                <img src="Assets/Image/whoo.png" alt="Unicef Image">
+                <img src="Assets/Image/gwcc.png" alt="Unicef Image">
+                <img src="Assets/Image/puree.png" alt="Unicef Image">
+            </div>
+
+        <!--=======
+            More
+        ==========-->
+        <div class="container" style="display: flex; flex-direction: row; align-items: center; padding: 10%;">
+            <div class="text-section" style="flex: 1; padding-right: 40px;">
+                <div class="text" style="font-size: 35px; margin-bottom: 20px;">Seamless Engagement</div>
+                <div class="text-body" style="font-size: 20px; font-weight: 400; margin-bottom: 20px;">
+                    Explore and Participate in Organizational<br> Events Effortlessly!
+                </div>
+                <button class="button-2" onclick="window.location.href=''" style="margin-right: 10px;">Try now</button>
+                <button class="button-1" onclick="window.location.href=''" style="background: none; color: #00bcd4;left: 23%; width: 150px; height: 40px;">Learn more</button>
+            </div>
+            <div class="image-container" style="flex: 1; display: flex; align-items: center; justify-content: center;">
+                <img src="Assets/Image/volunteer.png" style="width: 80%; max-width: 400px; height: auto; border-radius: 30px;" alt="Event Image">
+            </div>
+        </div>
+
+        <div class="container" style="display: flex; flex-direction: row; align-items: center; padding: 10%;">
+            <div class="image-container" style="flex: 1; display: flex; align-items: center; justify-content: center;">
+                    <img src="Assets/Image/volunteer.png" style="width: 80%; max-width: 400px; height: auto; border-radius: 30px;" alt="Event Image">
+                </div>
+                <div class="text-section" style="flex: 1; padding-right: 20px;">
+                    <div class="text" style="font-size: 35px; margin-bottom: 20px;">Seamless Engagement</div>
+                    <div class="text-body" style="font-size: 20px; font-weight: 400; margin-bottom: 20px;">
+                        Explore and Participate in Organizational<br> Events Effortlessly!
+                    </div>
+                    
+                    <button class="button-1" onclick="window.location.href=''" style="background: none; color: #00bcd4;left: 70%; width: 150px; height: 40px;">Learn more</button>
+                    <button class="button-2" onclick="window.location.href=''" style="margin-right: 10px;">Try now</button>
+                </div>
+            </div>
+        </div>
+
+        <!--=====================
+            Lower-Body Content
+        =========================-->
+        <div class="container" style="display: flex; flex-direction: row; align-items: center; padding: 10%; background: #EBFDFFFF;">
+            <div class="text-normal" style="top: 260%; left: 20%; font-size: 40px; text-align: center; font-weight: 700;">Leonardo da Vinci</div>
+            <div class="text-normal" style="top: 270%; left: 20%; font-size: 33px; text-align: center;">"Water is the driving force of all nature."</div>
+        </div>
+    
+
+        <div class="container" style=" top: 2769px; margin-left: 10%; width: 1176px; height: 358px; border-radius: 0px 8px 8px; background: #00BDD6FF; display: flex; align-items: center; justify-content: center; ">
+            <div class="text-normal" style="top: 300%; left: 20%; width: 460px; font-family: Epilogue; font-size: 48px; line-height: 68px; font-weight: 700; color: #FFFFFFFF;">Get Started!</div>
+            <div class="text-normal" style="top: 310%; left: 20%; font-size: 15px; ">Join us to unlock a world of possibilities! With<br>
+             exclusive access, you'll discover exciting events,<br>
+              impactful initiatives, and personalized insights. Don't <br>
+              miss out – sign up now and start your journey!</div>
+            <button class="button" style="top: 330%; margin-right: 46%;" onclick="window.location.href='./Register.php'">Get Started ></button>
+              <div class="image-container" style="display: flex; flex-direction: row; justify-content: center; align-items: center; height: 80vh;">
+                <img src="Assets/Image/Org1.png" style=" width: 90%; max-width: 1308px; height: auto; border-radius: 30px; margin-left: 80%;" alt="Event Image">
+            </div>
+
+        </div>
+    
+
+
+
+     </body> 
 
 
     <!-- Javascript implementation -->
